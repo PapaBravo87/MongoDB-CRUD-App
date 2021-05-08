82543f031ee9ef6688cb3eacd7764687064b4286
Prabodh Bharose <30743874+PapaBravo87@users.noreply.github.com>
2021-05-08 17:26:42 +0530

    remove() Bug Fix

--- myApp.js
+++ myApp.js
@@ -49,7 +49,7 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  Person.find( {name: personName} , function(err) {
+  Person.find( {name: personName} , function(err,personFound) {
     if(err) {
       console.log(err);
       return handleError(err);

1dc34c184f5513542800102bb182c2cd40746b5e
Prabodh Bharose <30743874+PapaBravo87@users.noreply.github.com>
2021-05-09 10:53:02 +0530

    Add Model.Remove() for Bulk Removal of Docs

--- myApp.js
+++ myApp.js
@@ -124,8 +124,13 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove(nameToRemove,(err,data) => {
+    if(err){
+      console.log(err);
+      return handleError(err);
+    }
+    done(null, data);
+  });
 };
 
 const queryChain = (done) => {
